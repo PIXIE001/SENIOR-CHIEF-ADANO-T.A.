568cde1fb33cd4b3cdf91abe36711718d82897f0
PIXIE001 <itscrazypixie@gmail.com>
2026-09-06 11:26:24 +0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -31,7 +31,7 @@ app.use(
 );
 
 const SUPABASE_URL = process.env.SUPABASE_URL;
-const SUPABASE_ANON_KEY = process.env.SUPABASE_ANON_KEY;
+const SUPABASE_SERVICE_ROLE_KEY = process.env.SUPABASE_SERVICE_ROLE_KEY;
 const JWT_SECRET = process.env.JWT_SECRET;
 
 function configurationReady() {
